76c6c5782ab123520e2ca0a91074a60c3f3f58cd
Doug Pfeil <dpfeil@gmail.com>
2020-08-08 23:40:02 -0400

    Add basic spreadsheet generation

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,3 +1,20 @@
+import Spreadsheet from './Spreadsheet';
 
-const container = document.getElementById("container");
-if(container !== null) container.innerHTML = "Hello, world";
+
+const addButton = (name: string, text: string, fn: any): void => {
+  var button = document.createElement("button");
+  button.id = name;
+  button.innerHTML = text;
+  button.addEventListener("click", fn);
+  document.getElementById("buttons")?.append(button);
+}
+
+
+const spreadsheet = new Spreadsheet();
+
+spreadsheet.getCells();
+spreadsheet.drawCells();
+
+
+addButton("addcolum","add column", spreadsheet.addColumn);
+addButton("addrow","add row", spreadsheet.addRow);
