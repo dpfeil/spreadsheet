173d4223f403f20588880787055421cb69de7bd8
Doug Pfeil <dpfeil@gmail.com>
2020-08-08 23:53:09 -0400

    Add key down, focus, blur event listeners to Cell

--- src/browser/Cell.ts
+++ src/browser/Cell.ts
@@ -12,9 +12,36 @@ class Cell {
     cell.className = "cell";
     cell.id = this.id;
     var input = document.createElement("input");
+    input.addEventListener("keydown", this.onKeyDown);
+    input.addEventListener("focus", this.onFocus);
+    input.addEventListener("blur", this.onBlur);
     input.value = this.value;
     cell.appendChild(input);
     return cell;
+  }
+
+  onFocus = (e: any) => {
+    e.target.value = this.value;
+    e.target.setSelectionRange(0, this.value.length);
+  }
+
+  onKeyDown = (e: any) => {
+    const val = e.target.value;
+    if(e.keyCode === 13) { // enter
+      this.value = val;
+      e.target.blur();
+    } else if(e.keyCode === 27) { // escape
+      e.target.value = this.value;
+      e.target.blur();
+    } else if(e.which === 9) { // tab (moves focus to next cell)
+      e.preventDefault();
+      e.stopPropagation();
+      (<HTMLElement>document.getElementById(this.id)?.nextElementSibling?.firstChild).focus()
+    }
+  }
+
+  onBlur = (e: any) => {
+
   }
 
 }
